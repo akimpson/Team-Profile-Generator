d4199bda0e80422788d2c918739e0d0eb8a72d11
Aisha Kimpson <akimp313@gmail.com>
2022-07-09 20:03:13 -0400

    start app & create manager

--- src/index.js
+++ src/index.js
@@ -18,3 +18,65 @@ const render = require("./src/page-template.js");
 // Create empty arrays for team and id as place holders
 const teamArr = [];
 const idArr = [];
+
+// Start application
+function initApp() {
+
+    // Prompt user to create a manager when application starts
+    function addManager() {
+        console.log("Start building your team profile");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What's manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter the team's manager's name.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What's the manager's ID?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a valid Manager's ID.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What's the manager's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Email address can't be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What's the manager's office number? (format: 111111111)",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a correct phone number.";
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamArr.push(manager);
+            idArr.push(answers.managerId);
+            addTeam();
+        });
+    }
